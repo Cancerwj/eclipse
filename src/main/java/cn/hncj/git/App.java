0e97c3c361b7701e53a08fef1c8f5e089eded9e3
Cancerwj <3267363172@qq.com>
2018-10-30 19:23:56 +0800

    commit a eclipse project

---
+++ src/main/java/cn/hncj/git/App.java
@@ -0,0 +1,13 @@
+package cn.hncj.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
